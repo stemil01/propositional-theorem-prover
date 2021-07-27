a06e584bba45f54b0494ed3e6a26e18269ca8373
stemil01 <stefan180501@gmail.com>
2021-07-27 09:34:08 +0200

    prototip za latex

--- latex.cpp
+++ latex.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+#include <fstream>
+#include "tree.hpp"
 #include "latex.hpp"
 using namespace std;
 
+// check type of arguments !!
+void generate_tex(Tnode* root, ofstream& file)
+{
 
+}
+
+ofstream tex_file(Tnode* root)
+{
+    ofstream file("tableaux.tex");
+    // insert header for latex file
+    file << "\\documentclass{article}\n\\begin{document}\n";
+    // ...
+    file << "\node{F , }"
+    generate_tex(root, file);
+
+    file << "\end{document}\n"; // file footer here
+    return file;
+}
