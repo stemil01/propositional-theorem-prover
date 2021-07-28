8a4a935ca6641af05a8212640729712c8a9cbe2b
unknown <toni.subota@gmail.com>
2021-07-28 16:57:46 +0200

    niss

---
+++ well_formed.hpp
@@ -0,0 +1,19 @@
+#ifndef _WELL_FORMED_HPP_
+#define _WELL_FORMED_HPP_
+
+void format(char* input);
+
+bool is_bin_symbol(char c);
+
+bool is_letter(char c);
+
+bool check_brackets(const char *input, int start, int end);
+
+bool embraced_brackets(const char* input, int start, int end);
+
+Node* create_tree(const char* input, int start, int end);
+
+// checks well form of input and creates a tree with it
+Node *check_well_form(char *input);
+
+#endif
